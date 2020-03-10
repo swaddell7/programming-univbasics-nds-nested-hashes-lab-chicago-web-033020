6c15bc34d07a1e22095dee94bb577d735a21760d
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 21:20:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,10 +25,10 @@ def literal_aoh
   # held in the constants
   
   literal_aoh = [
-    {name: "Don Gately", occupation: "Live-in Staff"}
-    {name: "Joelle van Dyne", occupation: "Radio Personality"}
-    {name: "Pat Monteseian", occupation: "Staff"}
-    {name: "Kate Gompert", occupation: "none"}
+    {name: "Don Gately", occupation: "Live-in Staff"},
+    {name: "Joelle van Dyne", occupation: "Radio Personality"},
+    {name: "Pat Monteseian", occupation: "Staff"},
+    {name: "Kate Gompert", occupation: "none"},
     {name: "Bruce Green", occupation: "Fan of Mildred"}
   ]
   
