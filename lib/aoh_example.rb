7cb9871ac9ad06aa8976fc59e9ae145b549a7e26
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 21:21:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,7 +28,7 @@ def literal_aoh
     {name: "Don Gately", occupation: "Live-in Staff"},
     {name: "Joelle van Dyne", occupation: "Radio Personality"},
     {name: "Pat Monteseian", occupation: "Staff"},
-    {name: "Kate Gompert", occupation: "none"},
+    {name: "Kate Gompert", occupation: "None"},
     {name: "Bruce Green", occupation: "Fan of Mildred"}
   ]
   
